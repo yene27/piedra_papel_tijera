60b62ae1cebd7dcd108cb7f961f12b9b1fb9378c
yene27 <yene27@yahoo.com>
2022-05-30 18:48:47 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+let mano = 0
+input.onGesture(Gesture.Shake, function () {
+    mano = randint(1, 3)
+    if (mano == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (mano == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (mano == 3) {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+basic.forever(function () {
+	
+})
